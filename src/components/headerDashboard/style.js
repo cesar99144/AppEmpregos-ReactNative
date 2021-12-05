d0787dce763f3449b6a7c9a4b7eae4ed7cb7d260
César Ferreira <cesarferreiraa67@gmail.com>
2021-12-04 21:48:37 -0300

    Estilos header

---
+++ src/components/headerDashboard/style.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export const Container = styled.View`
+    width: 100%;
+    background-color: #1C58F2;
+    padding-left: 14px;
+    height: 64px;
+    align-items: center;
+    flex-direction: row;
+    justify-content: space-between;
+`;
+
+export const MenuBotao = styled.TouchableOpacity`
+    height: 70px;
+    align-items: center;
+    flex-direction: row;
+`;
+
+export const Titulo = styled.Text`
+    font-size: 22px;
+    font-weight: bold;
+    color: white;
+    
+`;
+
+export const FiltroBotao = styled.TouchableOpacity`
+    height: 70px;
+    align-items: center;
+    flex-direction: row;
+    margin-right: 14px;
+`;
