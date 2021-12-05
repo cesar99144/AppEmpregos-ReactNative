2b8b3368f25e64d30dd95a7b83b4c83042958b52
César Ferreira <cesarferreiraa67@gmail.com>
2021-12-04 21:48:10 -0300

    Header para o dashboard (área de usuários logados)

---
+++ src/components/headerAuth/style.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const ContainerArea = styled.View`
+    width: 100%;
+    background-color: #1C58F2;
+    padding-left: 14px;
+    height: 64px;
+    align-items: center;
+    flex-direction: row;
+`;
+
+export const BotaoVoltar = styled.TouchableOpacity`
+    height: 70px;
+    align-items: center;
+    flex-direction: row;
+`;
+
+export const Titulo = styled.Text`
+    font-size: 22px;
+    font-weight: bold;
+    color: white;
+    margin-left: 20%;
+`;
