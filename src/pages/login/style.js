51014b992d6c5da71801d63edc97da3c820411cb
César Ferreira <cesarferreiraa67@gmail.com>
2021-12-04 21:49:46 -0300

    Create style.js

---
+++ src/pages/login/style.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const AreaDadosLogin = styled.View`
+    margin-top: 10%;
+`;
+
+export const BotaoLogin = styled.TouchableOpacity`
+    background-color: #393939;
+    align-items: center;
+    padding: 10px;
+    border-radius: 50px;
+    width: 100%;
+    height: 58px;
+    align-items: center;
+`;
