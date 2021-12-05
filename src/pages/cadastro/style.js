c400738a6908cff4ba3927005db5a8459711cb27
César Ferreira <cesarferreiraa67@gmail.com>
2021-12-04 21:48:59 -0300

    Create style.js

---
+++ src/pages/cadastro/style.js
@@ -0,0 +1,67 @@
+import styled from "styled-components";
+
+export const Container = styled.View`
+    flex: 1;
+`;
+
+export const AreaTextos = styled.View`
+    width: 100%;
+    margin-top: 5%;
+    padding: 8px 17px;
+`;
+
+export const Titulo = styled.Text`
+    font-size: 25px;
+    color: #393939;
+`;
+
+export const SubTitulo = styled.Text`
+    font-size: 45px;
+    color: #393939;
+    font-weight: bold;
+`;
+
+export const TextInfo = styled.Text`
+    font-size: 20px;
+    color: #909090;
+    margin-top: 5%;
+    font-weight: bold;
+`;
+
+export const ContainerForm = styled.View`
+    padding: 8px 17px;
+    
+`;
+
+export const AreaDados = styled.View`
+    
+    margin-bottom: 5%;
+`;
+
+export const Label = styled.Text`
+    font-size: 18px;
+    color: #393939;
+`;
+
+export const Input = styled.TextInput`
+    border: 1px solid white;
+    border-bottom-color: black;
+    font-size: 19px;
+    
+`;
+
+export const BotaoCadastrar = styled.TouchableOpacity`
+    background-color: #393939;
+    align-items: center;
+    padding: 10px;
+    border-radius: 50px;
+    width: 100%;
+    height: 58px;
+    align-items: center;
+`;
+
+export const TextoBotao = styled.Text`
+    font-size: 20px;
+    color: white;
+    font-weight: bold;
+`;
