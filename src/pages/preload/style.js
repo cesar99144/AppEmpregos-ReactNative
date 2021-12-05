e211fea6b9d5fb0367e8237d6220925c2fe13c58
César Ferreira <cesarferreiraa67@gmail.com>
2021-12-04 21:50:03 -0300

    Create style.js

---
+++ src/pages/preload/style.js
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+
+export const Container = styled.View`
+    flex: 1;
+    background-color: #1C58F2;
+`;
+
+export const Titulo = styled.Text`
+    font-size: 42px;
+    font-weight: bold;
+    text-align: center;
+    margin-top: 40%;
+    color: white;
+    font-family: 'Times New Roman';
+`;
+
+export const AreaVetorTop = styled.View`
+    width: 100%;
+    
+`;
+
+export const AreaTexto = styled.View`
+    width: 100%;
+    height: 20%;
+
+`;
+
+export const ImagemVetorTop = styled.Image`
+    width: 100%;
+    height: 100%;
+   
+`;
+
+export const AreaVetorBottom = styled.View`
+    width: 100%;
+    
+
+`;
+
+export const ImagemVetorBottom = styled.Image`
+    width: 100%;
+    height: 70%;
+`;
