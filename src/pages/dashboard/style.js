694999ab767727af3869a427c5274f04ee25020f
César Ferreira <cesarferreiraa67@gmail.com>
2021-12-04 21:49:36 -0300

    Create style.js

---
+++ src/pages/dashboard/style.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const Container = styled.View`
+    flex: 1;
+`;
+
+export const AreaTextos = styled.View`
+    width: 100%;
+    margin-top: 5%;
+    padding: 8px 17px;
+`;
+
+export const Titulo = styled.Text`
+    font-size: 25px;
+    color: #393939;
+`;
+
+export const SubTitulo = styled.Text`
+    font-size: 45px;
+    color: #393939;
+    font-weight: bold;
+`;
+
+export const TextInfo = styled.Text`
+    font-size: 20px;
+    color: #909090;
+    margin-top: 8%;
+    font-weight: bold;
+`;
