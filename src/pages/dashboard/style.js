00d6ca48b159525c2f78357f70f919cbf5758459
César Ferreira <cesarferreiraa67@gmail.com>
2021-12-06 21:26:16 -0300

    Estilos dos novos componentes (pesquisa e dados das vagas)

--- src/pages/dashboard/style.js
+++ src/pages/dashboard/style.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 export const Container = styled.View`
     flex: 1;
+    background-color: #FFFFFF;
 `;
 
 export const AreaTextos = styled.View`
@@ -26,4 +27,43 @@ export const TextInfo = styled.Text`
     color: #909090;
     margin-top: 8%;
     font-weight: bold;
+`;
+
+export const AreaPesquisa = styled.View`
+    width: 100%;
+    padding: 8px 17px;
+    flex-direction: row;
+    margin-top: 20px;
+`;
+
+export const Input = styled.TextInput`
+    background-color: #EEEEEE;
+    width: 80%;
+    height: 53px;
+    font-size: 20px;
+    padding: 10px;
+`;
+
+export const BotaoPesquisar = styled.TouchableOpacity`
+    background-color: #1C58F2;
+    height: 52px;
+    padding: 10px;
+    width: 15%;
+`;
+
+export const AreaVagas = styled.View`
+    width: 100%;
+    padding: 8px 17px;
+    margin-top: 3%;
+`;
+
+export const Titulovagas = styled.Text`
+    font-size: 20px;
+    color: #909090;
+    margin-top: 3%;
+    font-weight: bold;
+`;
+
+export const ListVagas = styled.FlatList`
+    
 `;
