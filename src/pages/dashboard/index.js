36264cbbbf3c657c70d624f15a1a90bed5bba5f2
César Ferreira <cesarferreiraa67@gmail.com>
2021-12-06 21:25:57 -0300

    Área de pesquisa e dados das vagas

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,13 +1,23 @@
 import React from "react";
+import {ScrollView} from "react-native";
 import { 
     Container,
     AreaTextos, 
     Titulo,
     SubTitulo,
-    TextInfo
+    TextInfo,
+    AreaPesquisa,
+    Input,
+    BotaoPesquisar,
+    AreaVagas,
+    Titulovagas,
+    ListVagas
 } from './style';
 
+import Icon from 'react-native-vector-icons/Feather';
+
 import HeaderDashboard from "../../components/headerDashboard";
+import CardVagas from "../../components/CardVagas";
 
 export default function Dashboard(){
     return(
@@ -17,8 +27,30 @@ export default function Dashboard(){
             <AreaTextos>
                 <Titulo>Olá, bem vindo(a) de volta</Titulo>
                 <SubTitulo>César</SubTitulo>
-                <TextInfo>O que você gostaria de fazer?</TextInfo>
             </AreaTextos>
+
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <AreaPesquisa>
+                    <Input placeholder="Pesquisar uma vaga" />
+                    <BotaoPesquisar>
+                        <Icon
+                            name="search"
+                            size={28}
+                            color="#FFF"
+                        />
+                    </BotaoPesquisar>
+                </AreaPesquisa>
+
+                <AreaVagas>
+                    <Titulovagas>Vagas recentes</Titulovagas>
+
+                    <ListVagas 
+                        data={[1,2,3,4,5,6,7,8,9,9]}
+                        renderItem={ ( { item }) => <CardVagas data={item} />}
+                    />
+                </AreaVagas>
+
+            </ScrollView>
         </Container>
     );
 }
