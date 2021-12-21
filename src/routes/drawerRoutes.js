a04d668108084874dd2eb7aaa1406d4af42b1021
César Ferreira <cesarferreiraa67@gmail.com>
2021-12-20 21:31:58 -0300

    Páginas de completar o cadastro após primeiro login

--- src/routes/drawerRoutes.js
+++ src/routes/drawerRoutes.js
@@ -4,6 +4,8 @@ import { createDrawerNavigator } from "@react-navigation/drawer";
 const Drawer = createDrawerNavigator();
 
 import Dashboard from "../pages/dashboard";
+import CompletePerfil from "../pages/completePerfil";
+import ConcluirCadastro from "../pages/concluirCadastro";
 
 export default function DrawerRoutes({route: {params}}){
     return(
@@ -13,6 +15,15 @@ export default function DrawerRoutes({route: {params}}){
                 component={Dashboard}
                 initialParams={params}
             />
+            <Drawer.Screen 
+                name="CompletePerfil"
+                component={CompletePerfil}
+            />
+            <Drawer.Screen 
+                name="ConcluirCadastro"
+                component={ConcluirCadastro}
+            />
+
         </Drawer.Navigator>
     );
 }
