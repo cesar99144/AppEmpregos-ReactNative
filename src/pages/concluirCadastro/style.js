5d407e75272346ac0c24035359e61f566f28f5e5
César Ferreira <cesarferreiraa67@gmail.com>
2021-12-20 21:32:27 -0300

    Create style.js

---
+++ src/pages/concluirCadastro/style.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Container = styled.View`
+    flex: 1;
+`;
+
+export const AreaTextos = styled.View`
+    width: 100%;
+    margin-top: 2%;
+    padding: 8px 17px;
+`;
+
+export const TextInfo = styled.Text`
+    font-size: 20px;
+    color: #909090;
+    margin-top: 2%;
+    font-weight: bold;
+`;
