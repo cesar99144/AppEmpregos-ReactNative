811aaf444f41468807afa65d9c240e4e8985c6e8
César Ferreira <cesarferreiraa67@gmail.com>
2021-12-20 21:32:46 -0300

    Create style.js

---
+++ src/pages/completePerfil/style.js
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+
+export const Container = styled.View`
+    flex: 1;
+    background: #1C58F2;
+    align-items: center;
+`;
+
+export const AreaTexto = styled.View`
+    padding: 14px;
+    width: 90%;
+    text-align: center;
+    align-items: center;
+    margin-top: 12%;
+`;
+
+export const Titulo = styled.Text`
+    font-size: 30px;
+    color: #FFFFFF;
+    
+    font-weight: bold;
+`;
+
+export const AreaBotao = styled.View`
+    padding: 14px;
+    width: 90%;
+    text-align: center;
+    align-items: center;
+    margin-top: 5%
+`;
+
+export const BotaoConcluirPerfil = styled.TouchableOpacity`
+    background-color: #393939;
+    align-items: center;
+    padding: 10px;
+    border-radius: 50px;
+    width: 100%;
+    height: 58px;
+    align-items: center;
+`;
