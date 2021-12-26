71832208673cca98ef771d0e2856f58390f298c1
César Ferreira <cesarferreiraa67@gmail.com>
2021-12-25 21:59:46 -0300

    Organizando rotas

--- src/routes/drawerRoutes.js
+++ src/routes/drawerRoutes.js
@@ -6,14 +6,9 @@ const Drawer = createDrawerNavigator();
 import Dashboard from "../pages/dashboard";
 import StackDash from "./stackDash";
 
-export default function DrawerRoutes({route: {params}}){
+export default function DrawerRoutes(){
     return(
         <Drawer.Navigator>
-            <Drawer.Screen 
-                name="Dashboard"
-                component={Dashboard}
-                initialParams={params}
-            />
             <Drawer.Screen 
                 name="StackDash"
                 component={StackDash}
--- src/routes/stackDash.js
+++ src/routes/stackDash.js
@@ -3,19 +3,25 @@ import { createStackNavigator } from "@react-navigation/stack";
 
 const Stack = createStackNavigator();
 
+import Dashboard from "../pages/dashboard";
 import VizualizarVaga from "../pages/vizualizarVaga/index";
 
-export default function StackDash({route: {params}}){
+export default function StackDash(){
     return(
         <Stack.Navigator>
+            <Stack.Screen 
+                name="Dashboard"
+                component={Dashboard}
+                options={{
+                    headerShown: false
+                }}
+            />
             <Stack.Screen
                 name="VizualizarVaga"
                 component={VizualizarVaga}
                 options={{
                     headerShown: false
                 }}
-                initialParams={params}
-                //getId={({ params }) => params}
             />
         </Stack.Navigator>
     );
