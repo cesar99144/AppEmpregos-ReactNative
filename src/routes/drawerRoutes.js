6c477f939bce19816955524fbe4ea1048cb67caf
César Ferreira <cesarferreiraa67@gmail.com>
2021-12-29 21:43:29 -0300

    Update drawerRoutes.js

--- src/routes/drawerRoutes.js
+++ src/routes/drawerRoutes.js
@@ -5,17 +5,22 @@ const Drawer = createDrawerNavigator();
 
 import StackDash from "./stackDash";
 import ListTodasVagas from "../pages/listTodasVagas";
+import Candidaturas from "../pages/candidaturas";
 
 export default function DrawerRoutes(){
     return(
         <Drawer.Navigator>
             <Drawer.Screen 
-                name="StackDash"
+                name="HomeDash"
                 component={StackDash}
             />
             <Drawer.Screen 
-                name="ListTodasVagas"
+                name="Todas as vagas"
                 component={ListTodasVagas}
+            />
+            <Drawer.Screen 
+                name="Candidaturas"
+                component={Candidaturas}
             />
         </Drawer.Navigator>
     );
